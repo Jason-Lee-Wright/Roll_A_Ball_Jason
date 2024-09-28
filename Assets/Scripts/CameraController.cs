007ecdb03d03fc140c20cf49389373d0c54d8f2b
Jason <jason974448@gmail.com>
2024-09-28 17:54:17 -0300

    cleaned up code

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    public float distance = 5.0f; // Distance from the player
-    public float xSpeed = 120.0f;  // Speed of rotation around the player
-    public float ySpeed = 120.0f;  // Speed of vertical rotation
+    public float distance = 5.0f; 
+    public float xSpeed = 120.0f;  
+    public float ySpeed = 120.0f;
 
-    private float x = 0.0f; // Horizontal angle
-    private float y = 0.0f; // Vertical angle
+    private float x = 0.0f; 
+    private float y = 0.0f; 
 
-    // Start is called before the first frame update
     void Start()
     {
         Vector3 angles = transform.eulerAngles;
@@ -20,15 +19,14 @@ public class CameraController : MonoBehaviour
         y = angles.x;
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
-        if (Input.GetMouseButton(1)) // Right mouse button to rotate
+        if (Input.GetMouseButton(1))
         {
             x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
             y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
 
-            y = Mathf.Clamp(y, -20, 80); // Clamp vertical angle
+            y = Mathf.Clamp(y, -20, 80); 
         }
 
         Quaternion rotation = Quaternion.Euler(y, x, 0);
