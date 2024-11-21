2ad36284668aa2486f43d149a1df4cdcce39655d
Jason <jason974448@gmail.com>
2024-11-21 18:55:49 -0400

    I updated the respawn logic for the water

--- Assets/Scripts/Water kill.cs
+++ Assets/Scripts/Water kill.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
 
-public class Waterkill : PlayerController
+public class Waterkill : MonoBehaviour
 {
     public GameObject Player;
+
+    Vector3 Spawn;
+
+    private void Start()
+    {
+        Spawn = Player.transform.position;
+    }
 
     private void OnTriggerEnter()
     {
